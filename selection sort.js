8ef539f30bdfad1ddd1bffcf4aaec860cd3167f4
Rudraksh2 <68378079+Rudraksh2@users.noreply.github.com>
2020-10-12 22:14:16 +0530

    Add files via upload

--- selection sort.js
+++ selection sort.js
@@ -6,22 +6,24 @@ function selectionSort(){
   let n = arr.length;
   let s = getspeed();
   let speed_var = 50/s;
-  let k;
 
   for(let i=0;i<n-1;i++){
+      let k = i;
 
     setTimeout(() => {
       arr[i].style.backgroundColor = 'red';
     }, time = time + speed_var);
 
-    for(let j=k=i;j<n;j++){
+    for(let j=i+1;j<n;j++){
 
       setTimeout(() => {
         arr[j+1].style.backgroundColor = 'yellow';
       }, time = time + speed_var);
       setTimeout(function(){
       if(height[j] < height[k]){
+        if(k!=i){
         arr[k].style.backgroundColor = 'blue';
+        }
         k = j;
         arr[k].style.backgroundColor = 'red';
       }
@@ -37,7 +39,12 @@ function selectionSort(){
       arr[i].style.height = `${height[i]}px`;
       arr[k].style.height = `${height[k]}px`;
 
+      arr[k].style.backgroundColor = "blue";
+
       arr[i].style.backgroundColor = "green";
     },time = time + speed_var);
     }
+    setTimeout(function(){
+      arr[n-1].style.backgroundColor = "green";
+    },time = time + speed_var);
 }
